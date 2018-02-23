360d720d8082e8a9893b483df0d1bb5d34e2317c
raphael-jorge <raphaeljjj@gmail.com>
2018-02-23 15:32:12 -0300

    feat: Create api module
      - Add uuid to create ids
    
      - Add api test module

---
+++ readable/src/__tests__/PostsAPI.test.js
@@ -0,0 +1,297 @@
+import * as Api from '../util/PostsAPI';
+
+global.fetch = jest.fn(
+  () => Promise.resolve({ json: () => {} })
+);
+
+const util = {
+  apiUrl: 'http://localhost:3001',
+
+  getFetchConfig: () => {
+    // Seleciona os argumentos da última chamada feita a fetch
+    const fetchCallArgs = global.fetch.mock.calls.slice(-1)[0];
+    const [fetchCallUrl, fetchCallConfig] = fetchCallArgs;
+
+    // Configura url e body
+    fetchCallConfig.url = fetchCallUrl;
+
+    if (fetchCallConfig.body) {
+      fetchCallConfig.body = JSON.parse(fetchCallConfig.body);
+    }
+
+    return fetchCallConfig;
+  },
+
+  verifyHeaders: (fetchCallConfig) => {
+    expect(fetchCallConfig.headers).toBeDefined();
+    expect(fetchCallConfig.headers.Authorization).toBeDefined();
+    expect(fetchCallConfig.headers.Accept).toBeDefined();
+    expect(fetchCallConfig.headers['Content-Type']).toBeDefined();
+  },
+
+  verifyMethod: (expectedMethod, fetchCallConfig) => {
+    if (expectedMethod === 'GET') {
+      expect(fetchCallConfig.method).not.toBeDefined();
+    } else {
+      expect(fetchCallConfig.method).toBe(expectedMethod);
+    }
+  },
+};
+
+afterEach(() => {
+  global.fetch.mockClear();
+});
+
+
+describe('API', () => {
+  describe('GET methods', () => {
+    const expectedMethod = 'GET';
+
+    it('get all categories', () => {
+      return Api.get.categories().then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/categories`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('get all posts', () => {
+      return Api.get.posts().then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('get all posts for a specific category', () => {
+      const category = 'testCategory';
+
+      return Api.get.posts(category).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/${category}/posts`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('get a specific post', () => {
+      const testPostId = 'testId';
+
+      return Api.get.post(testPostId).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('get all comments for a specific post', () => {
+      const testPostId = 'testId';
+
+      return Api.get.postComments(testPostId).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}/comments`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('get details for a specific comment', () => {
+      const testCommentId = 'testId';
+      return Api.get.comment(testCommentId).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments/${testCommentId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+  });
+
+
+  describe('POST methods', () => {
+    const expectedMethod = 'POST';
+
+    it('add a new post', () => {
+      const testPost = {
+        title: 'testTitle',
+        body: 'testBody',
+        author: 'testAuthor',
+        category: 'testCategory',
+      };
+
+      return Api.create.post(testPost).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.id).toBeDefined();
+        expect(fetchCallConfig.body.timestamp).toBeDefined();
+        expect(fetchCallConfig.body.title).toBe(testPost.title);
+        expect(fetchCallConfig.body.body).toBe(testPost.body);
+        expect(fetchCallConfig.body.author).toBe(testPost.author);
+        expect(fetchCallConfig.body.category).toBe(testPost.category);
+      });
+    });
+
+    it('vote to increase a post score', () => {
+      const testPostId = 'testId';
+      const testVote = 1;
+      return Api.create.voteOnPost(testPostId, testVote).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.option).toBe('upVote');
+      });
+    });
+
+    it('vote to decrease a post score', () => {
+      const testPostId = 'testId';
+      const testVote = -1;
+      return Api.create.voteOnPost(testPostId, testVote).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.option).toBe('downVote');
+      });
+    });
+
+    it('add a new comment to a post', () => {
+      const testPostId = 'testId';
+      const testComment = {
+        author: 'testAuthor',
+        body: 'testBody',
+      };
+
+      return Api.create.comment(testPostId, testComment).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.id).toBeDefined();
+        expect(fetchCallConfig.body.timestamp).toBeDefined();
+        expect(fetchCallConfig.body.parentId).toBe(testPostId);
+        expect(fetchCallConfig.body.author).toBe(testComment.author);
+        expect(fetchCallConfig.body.body).toBe(testComment.body);
+      });
+    });
+
+    it('vote to increase a comment score', () => {
+      const testCommentId = 'testId';
+      const testVote = 1;
+
+      return Api.create.voteOnComment(testCommentId, testVote).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments/${testCommentId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.option).toBe('upVote');
+      });
+    });
+
+    it('vote to decrease a comment score', () => {
+      const testCommentId = 'testId';
+      const testVote = -1;
+
+      return Api.create.voteOnComment(testCommentId, testVote).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments/${testCommentId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.option).toBe('downVote');
+      });
+    });
+  });
+
+
+  describe('PUT methods', () => {
+    const expectedMethod = 'PUT';
+
+    it('edit a post', () => {
+      const testPostId = 'testId';
+      const testUpdatedPost = {
+        title: 'testTitle',
+        body: 'testBody',
+      };
+
+      return Api.update.post(testPostId, testUpdatedPost).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.title).toBe(testUpdatedPost.title);
+        expect(fetchCallConfig.body.body).toBe(testUpdatedPost.body);
+      });
+    });
+
+    it('edit a comment', () => {
+      const testCommentId = 'testId';
+      const testUpdatedComment = {
+        body: 'testBody',
+      };
+
+      return Api.update.comment(testCommentId, testUpdatedComment).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments/${testCommentId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+
+        expect(fetchCallConfig.body.timestamp).toBeDefined();
+        expect(fetchCallConfig.body.body).toBe(testUpdatedComment.body);
+      });
+    });
+  });
+
+
+  describe('DELETE methods', () => {
+    const expectedMethod = 'DELETE';
+
+    it('delete a post', () => {
+      const testPostId = 'testId';
+
+      return Api.del.post(testPostId).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/posts/${testPostId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+
+    it('delete a comment', () => {
+      const testCommentId = 'testId';
+
+      return Api.del.comment(testCommentId).then(() => {
+        const fetchCallConfig = util.getFetchConfig();
+
+        expect(fetchCallConfig.url).toBe(`${util.apiUrl}/comments/${testCommentId}`);
+        util.verifyHeaders(fetchCallConfig);
+        util.verifyMethod(expectedMethod, fetchCallConfig);
+      });
+    });
+  });
+});
---
+++ readable/src/util/PostsAPI.js
@@ -0,0 +1,214 @@
+/** @module PostsAPI */
+import uuid from 'uuid/v4';
+
+const api = 'http://localhost:3001';
+
+let token = localStorage.token;
+if (!token) {
+  token = Math.random().toString(36).substr(-8);
+  localStorage.token = token;
+}
+
+const headers = {
+  Authorization: token,
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+
+/**
+ * @description Métodos para buscar dados no servidor.
+ * @namespace get
+ */
+export const get = {
+  /**
+   * @description Busca categorias no servidor.
+   * @return {Promise} Retorna uma array com as categorias obtidas quando resolvida.
+   */
+  categories: () =>
+    fetch(`${api}/categories`, { headers })
+      .then((res) => res.json()),
+
+  /**
+   * @description Busca posts no servidor.
+   * @param {string} [category] Uma categoria específica de posts.
+   * @return {Promise} Retorna uma array com os posts obtidos quando resolvida.
+   */
+  posts: (category) => {
+    let path = '/posts';
+    if (category) {
+      path = `/${category}` + path;
+    }
+    return fetch(`${api}${path}`, { headers })
+      .then((res) => res.json());
+  },
+
+  /**
+   * @description Busca um post específico no servidor.
+   * @param  {string} postId O id do post.
+   * @return {Promise} Retorna o objeto post obtido quando resolvida.
+   */
+  post: (postId) =>
+    fetch(`${api}/posts/${postId}`, { headers })
+      .then((res) => res.json()),
+
+  /**
+   * @description Busca comentários de um post específico no servidor.
+   * @param  {string} postId O id do post.
+   * @return {Promise} Retorna uma array com os comentários obtidos.
+   */
+  postComments: (postId) =>
+    fetch(`${api}/posts/${postId}/comments`, { headers })
+      .then((res) => res.json()),
+
+  /**
+   * @description Busca um comentário específico no servidor.
+   * @param  {string} commentId O id do comentário.
+   * @return {Promise} Retorna o objeto comentário obtido quando resolvida.
+   */
+  comment: (commentId) =>
+    fetch(`${api}/comments/${commentId}`, { headers })
+      .then((res) => res.json()),
+};
+
+
+/**
+ * @description Métodos para criar dados no servidor.
+ * @namespace create
+ */
+export const create = {
+  /**
+   * @description Cria um novo post no servidor.
+   * @param {object} newPostBody Um objeto com as informações do novo post.
+   * @param {string} newPostBody.title O título do post.
+   * @param {string} newPostBody.body A mensagem do post.
+   * @param {string} newPostBody.author O autor do post.
+   * @param {string} newPostBody.category A categoria do post.
+   * @return {Promise} Retorna o objeto post criado no servidor quando resolvida.
+   */
+  post: (newPostBody) => {
+    newPostBody.id = uuid();
+    newPostBody.timestamp = Date.now();
+    return fetch(`${api}/posts`, {
+      headers: { ...headers },
+      method: 'POST',
+      body: JSON.stringify(newPostBody),
+    }).then((res) => res.json());
+  },
+
+  /**
+   * @description Cria um novo voto em um post específico.
+   * @param {string} postId O id do post.
+   * @param {number} vote Se vote>0 adiciona um voto se não diminui um voto.
+   * @return {Promise} Retorna o objeto post atualizado quando resolvida.
+   */
+  voteOnPost: (postId, vote) => {
+    const voteStr = vote > 0 ? 'upVote' : 'downVote';
+    return fetch(`${api}/posts/${postId}`, {
+      headers: { ...headers },
+      method: 'POST',
+      body: JSON.stringify({ option: voteStr }),
+    }).then((res) => res.json());
+  },
+
+  /**
+   * @description Cria um novo comentário vinculado a um post no servidor.
+   * @param {string} postId O id do post a receber o comentário.
+   * @param {object} newComment Um objeto com as informações do novo comentário.
+   * @param {string} newComment.body A mensagem do comentário.
+   * @param {string} newComment.author O autor do comentário.
+   * @return {Promise} Retorna o objeto comentário criado no servidor quando resolvida.
+   */
+  comment: (postId, newComment) => {
+    newComment.id = uuid();
+    newComment.timestamp = Date.now();
+    newComment.parentId = postId;
+    return fetch(`${api}/comments`, {
+      headers: { ...headers },
+      method: 'POST',
+      body: JSON.stringify(newComment),
+    }).then((res) => res.json());
+  },
+
+  /**
+   * @description Cria um novo voto em um comentário específico.
+   * @param {string} commentId O id do comentário.
+   * @param {number} vote Se vote>0 adiciona um voto se não diminui um voto.
+   * @return {Promise} Retorna o objeto comentário atualizado quando resolvida.
+   */
+  voteOnComment: (commentId, vote) => {
+    const voteStr = vote > 0 ? 'upVote' : 'downVote';
+    return fetch(`${api}/comments/${commentId}`, {
+      headers: { ...headers },
+      method: 'POST',
+      body: JSON.stringify({ option: voteStr }),
+    }).then((res) => res.json());
+  },
+};
+
+
+/**
+ * @description Métodos para atualizar dados no servidor.
+ * @namespace update
+ */
+export const update = {
+  /**
+   * @description Atualiza um post no servidor.
+   * @param {string} postId O id do post a ser atualizado.
+   * @param {object} updatedData Um objeto com as informações do post atualizadas.
+   * @param {string} updatedData.title O título atualizado do post.
+   * @param {string} updatedData.body A mensagem atualizada do post.
+   * @return {Promise} Retorna o objeto post atualizado quando resolvida.
+   */
+  post: (postId, updatedData) =>
+    fetch(`${api}/posts/${postId}`, {
+      headers: { ...headers },
+      method: 'PUT',
+      body: JSON.stringify(updatedData),
+    }).then((res) => res.json()),
+
+  /**
+   * @description Atualiza um comentário no servidor.
+   * @param {string} commentId O id do comentário a ser atualizado.
+   * @param {object} updatedData Um objeto com as informações do comentário atualizadas.
+   * @param {string} updatedData.body A mensagem atualizada do comentário.
+   * @return {Promise} Retorna o objeto comentário atualizado quando resolvida.
+   */
+  comment: (commentId, updatedData) => {
+    updatedData.timestamp = Date.now();
+    return fetch(`${api}/comments/${commentId}`, {
+      headers: { ...headers },
+      method: 'PUT',
+      body: JSON.stringify(updatedData),
+    }).then((res) => res.json());
+  },
+};
+
+
+/**
+ * @description Métodos para apagar dados do servidor.
+ * @namespace del
+ */
+export const del = {
+  /**
+   * @description Apaga um post específico do servidor.
+   * @param {string} postId O id do post.
+   * @return {Promise} Retorna o objeto post atualizado quando resolvida.
+   */
+  post: (postId) =>
+    fetch(`${api}/posts/${postId}`, {
+      headers: { ...headers },
+      method: 'DELETE',
+    }).then((res) => res.json()),
+
+  /**
+   * @description Apaga um comentário específico do servidor.
+   * @param {string} commentId O id do comentário.
+   * @return {Promise} Retorna o objeto comentário atualizado quando resolvida.
+   */
+  comment: (commentId) =>
+    fetch(`${api}/comments/${commentId}`, {
+      headers: { ...headers },
+      method: 'DELETE',
+    }).then((res) => res.json()),
+};
