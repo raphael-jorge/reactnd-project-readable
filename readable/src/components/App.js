5806844f345669cf1d7c33cf82092966e5c797de
raphael-jorge <raphaeljjj@gmail.com>
2018-02-27 21:50:33 -0300

    feat: Add router
      - Add react router dom and history
    
      - Set history to listen for url changes and dispatch actions to update
        the redux state

--- readable/src/components/App.js
+++ readable/src/components/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import routes from '../routes';
 
 class App extends Component {
   render() {
     return (
-      <header>
-        <h1>Readable</h1>
-      </header>
+      <div>
+
+        <Route exact path={routes.root} render={() => (
+          <p>Root</p>
+        )}/>
+
+        <Route exact path={routes.category} render={({ match }) => {
+          const category = match.params.category;
+          return (
+            <p>{category}</p>
+          );
+        }}/>
+
+        <Route exact path={routes.post} render={({ match }) => {
+          const category = match.params.category;
+          const postId = match.params.postId;
+          return (
+            <p>{category} ({postId})</p>
+          );
+        }}/>
+
+      </div>
     );
   }
 }
--- readable/src/index.js
+++ readable/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import configureStore from './store';
 import { fetchCategories } from './actions/categories';
+import setRouteState from './actions/routes';
 import './index.css';
 
 const store = configureStore();
 store.dispatch(fetchCategories());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+history.listen((location) => store.dispatch(setRouteState(history.location)));
+store.dispatch(setRouteState(history.location));
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
