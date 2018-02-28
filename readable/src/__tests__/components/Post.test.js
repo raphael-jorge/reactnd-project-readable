9fec77e8620bcc212371e305d3bf8300cb9bf518
raphael-jorge <raphaeljjj@gmail.com>
2018-02-28 20:59:26 -0300

    feat: Create Post component
      - Add tests for it

---
+++ readable/src/__tests__/components/Post.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Post from '../../components/Post';
+
+const setup = (propOverrides) => {
+  const props = Object.assign({
+    postData: {
+      id: 'testId',
+      category: 'testCategory',
+      timestamp: Date.now(),
+      title: '',
+      author: '',
+      body: '',
+    },
+    maxBodyLength: undefined,
+  }, propOverrides);
+
+  const post = shallow(<Post {...props} />);
+
+  return {
+    props,
+    post,
+  };
+};
+
+describe('<Post />', () => {
+  it('renders without crashing', () => {
+    const { post } = setup();
+    expect(post.find('.post').length).toBe(1);
+    expect(post.find('.post-info').length).toBe(1);
+    expect(post.find('.post-title').length).toBe(1);
+    expect(post.find('.post-body').length).toBe(1);
+  });
+
+  it('renders a link to the post page', () => {
+    const { post, props } = setup();
+    const expectedToProp = `/${props.postData.category}/${props.postData.id}`;
+    expect(post.find('Link').prop('to')).toBe(expectedToProp);
+  });
+
+  it('crops the body length if the maxBodyLength prop is set', () => {
+    const stringFiller = '-';
+    const continueMark = '...';
+    const testBodyLength = 100;
+    const testBody = Array(testBodyLength + 1).join(stringFiller);
+
+    // Limite maior que o body length
+    let maxBodyLength = 150;
+    const { post } = setup({ postData: { body: testBody }, maxBodyLength });
+    let expectedRenderedBody = testBody;
+
+    expect(post.find('.post-body').text()).toBe(expectedRenderedBody);
+
+    // Limite menor que o body length
+    maxBodyLength = 50;
+    post.setProps({ maxBodyLength });
+    expectedRenderedBody = Array(maxBodyLength - continueMark.length + 1)
+      .join(stringFiller) + continueMark;
+
+    expect(post.find('.post-body').text()).toBe(expectedRenderedBody);
+  });
+});
