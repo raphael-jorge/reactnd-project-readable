b6c71001e522a6c53948cd36551cd5cdd2d0c5c1
raphael-jorge <raphaeljjj@gmail.com>
2018-03-24 19:40:56 -0300

    feat: Remove logger middleware from redux store

--- readable/src/store/index.js
+++ readable/src/store/index.js
@@ -2,20 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  const result = next(action);
-  console.info('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(thunk, logger))
+    composeEnhancers(applyMiddleware(thunk))
   );
 }
